5b41c9f2b049f387566f7d123fa07733501bac2b
Stephen Haffner <dev@kd8zev.net>
2016-12-05 13:26:57 -0500

    Chown the logdir and set pemissions to 600

--- backend/functions.sh
+++ backend/functions.sh
@@ -38,6 +38,8 @@ export DBDIR LOGDIR PROGDIR CMDLOG REPCONF REPLOGSEND REPLOGRECV MSGQUEUE SSHPRO
 # Create the logdir
 if [ ! -d "$LOGDIR" ] ; then
    mkdir -p ${LOGDIR}
+   chown -R root:root ${LOGDIR}
+   chmod 600 ${LOGDIR}
 fi
 
 uname -r | grep -q 10.0
